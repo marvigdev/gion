db3cf6524f41dda0b3e35da302b966d479ba82c5
Vinícius <hello@marvig.dev>
2022-05-17 12:34:48 +0000

    chore(back): setup koa

---
+++ backend/src/app.ts
@@ -0,0 +1,12 @@
+import Koa from 'koa';
+import json from 'koa-json';
+import bodyParser from 'koa-bodyparser';
+import KoaLogger from 'koa-logger';
+
+const app = new Koa();
+
+app.use(KoaLogger());
+app.use(json());
+app.use(bodyParser());
+
+export default app;
