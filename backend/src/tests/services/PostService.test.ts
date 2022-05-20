5142fff48a6e3dd8c114029465a6e518bb2e80fc
Vinícius <hello@marvig.dev>
2022-05-20 19:22:02 -0300

    tests(back): add `PostService.create` tests

--- backend/src/tests/services/PostService.test.ts
+++ backend/src/tests/services/PostService.test.ts
@@ -2,23 +2,28 @@ import createTestDatabase, {
   TestDatabase,
 } from '../../helpers/createTestDatabase';
 import Post, { PostI } from '../../models/Post';
-import PostService from '../../services/PostService';
+import PostService, { CreatePostProps } from '../../services/PostService';
+
+let database: TestDatabase;
+
+beforeAll(async () => {
+  database = await createTestDatabase();
+  await database.connect();
+});
+
+afterAll(async () => {
+  await database.close();
+});
 
 describe('PostService.getById', () => {
-  let database: TestDatabase;
   let post1: PostI;
   let post2: PostI;
 
-  beforeAll(async () => {
-    database = await createTestDatabase();
-    await database.connect();
+  afterAll(async () => database.clear());
 
+  beforeAll(async () => {
     post1 = await Post.create({ title: 'Post 1', content: 'Content 1' });
     post2 = await Post.create({ title: 'Post 2', content: 'Content 2' });
-  });
-
-  afterAll(async () => {
-    await database.close();
   });
 
   test('should return an empty array on empty arguments', async () => {
@@ -54,5 +59,47 @@ describe('PostService.getById', () => {
     expect(request[0].title).toEqual(post2.title);
     expect(request[0].content).toEqual(post2.content);
     expect(request[0].identifier).toEqual(post2.identifier);
+  });
+});
+
+describe('PostService.create', () => {
+  let createdPost: PostI;
+
+  afterAll(async () => database.clear());
+
+  test('no post must exist before create', async () => {
+    const allPosts = await Post.find({});
+    expect(allPosts).toHaveLength(0);
+  });
+
+  test('post must be correctly inserted in database', async () => {
+    const payload: CreatePostProps = {
+      title: 'Example title',
+      content: 'Example content',
+    };
+
+    createdPost = await PostService.create(payload);
+
+    const [savedPost] = await PostService.getById(createdPost.identifier);
+
+    expect(savedPost).not.toBeUndefined();
+    expect(savedPost.title).toEqual(payload.title);
+    expect(savedPost.content).toEqual(payload.content);
+  });
+
+  test('post must start with default title if none is given', async () => {
+    const payload: CreatePostProps = {
+      content: 'This is another content',
+    };
+
+    createdPost = await PostService.create(payload);
+
+    // expect 2 documents to exist in this test
+    const allPosts = await Post.find({});
+    expect(allPosts).toHaveLength(2);
+
+    const [savedPost] = await PostService.getById(createdPost.identifier);
+    expect(savedPost.title).toEqual('Untitled post');
+    expect(savedPost.content).toEqual(payload.content);
   });
 });
