99716af7b9366fb3878ec8e0257e6fadc19a5e60
Vinícius <hello@marvig.dev>
2022-05-20 19:22:20 -0300

    feat(back): implement `PostService.create`

--- backend/src/services/PostService.ts
+++ backend/src/services/PostService.ts
@@ -1,9 +1,20 @@
 import Post, { PostI } from '../models/Post';
 
+export interface CreatePostProps {
+  title?: string;
+  content: string;
+}
+
 abstract class PostService {
   static async getById(...posts: string[]): Promise<PostI[]> {
     const foundPosts = await Post.find({ identifier: { $in: posts } });
     return foundPosts;
+  }
+
+  static async create({ title, content }: CreatePostProps): Promise<PostI> {
+    const newPost = new Post({ title, content });
+    await newPost.save();
+    return newPost;
   }
 }
 
